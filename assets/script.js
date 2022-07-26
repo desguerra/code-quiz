c75aae3f0550b1625901a142382da7a36dc28f11
desguerra <ddesguerra@ucdavis.edu>
2022-07-26 00:38:39 -0700

    start quiz button functionality and show first question

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,37 @@
-var startQuiz = document.getElementById("start-game");
-
-var currentQuestion = document.getElementById("question");
-var choicesList = document.getElementById("choices");
-var result = document.getElementById("result");
-
 var title = document.getElementById("title");
 var instructions = document.getElementById("instructions");
+var startButton = document.getElementById("start-game");
+
+var currentQuestion = document.getElementById("question");
+// list ul that displays answer choices
+var choicesList = document.getElementById("choices");
+var result = document.getElementById("result");
 
 function setUp() {
     title.textContent = "Coding Quiz Challenge";
     instructions.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
 };
 
-// var instructions = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
-
-function displayQuiz() {
-    instructions.textContent = "";
+function hideInstructions() {
     title.textContent = "";
+    instructions.textContent = "";
+    startButton.style.display = "none";
+    startQuiz();
 };
 
-startQuiz.addEventListener("click", displayQuiz);
+function startQuiz() {
+    // show current question
+    currentQuestion.textContent = "Question 1";
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "";
+
+    listItemEl.textContent = "answer option 1";
+
+    choicesList.appendChild(listItemEl);
+
+};
+
+startButton.addEventListener("click", hideInstructions);
 
 setUp();
